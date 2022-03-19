c2320c6255d65239fb94b447e191b36f1a98ef6a
John Hilasaca <89051327+Jaalya@users.noreply.github.com>
2022-03-19 16:54:34 -0500

    Creación clase archivoXML y modificación interfaz gráfica

--- FormMain.Designer.cs
+++ FormMain.Designer.cs
@@ -34,18 +34,24 @@ namespace Diccionario_Personal
             this.tbxPalabra = new System.Windows.Forms.TextBox();
             this.tbxTraduccion = new System.Windows.Forms.TextBox();
             this.gbInsertar = new System.Windows.Forms.GroupBox();
-            this.lblPalabra = new System.Windows.Forms.Label();
-            this.lblTraduccion = new System.Windows.Forms.Label();
+            this.rtbIdea = new System.Windows.Forms.RichTextBox();
             this.lbl_Idea = new System.Windows.Forms.Label();
+            this.lblTraduccion = new System.Windows.Forms.Label();
+            this.lblPalabra = new System.Windows.Forms.Label();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.archivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.verToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.preferenciasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.guardarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.salirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.verToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.preferenciasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.configuracionesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.ayudaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.rtbIdea = new System.Windows.Forms.RichTextBox();
+            this.cbxIdioma = new System.Windows.Forms.ComboBox();
+            this.lbl_Idioma = new System.Windows.Forms.Label();
+            this.lblTipo = new System.Windows.Forms.Label();
+            this.cbxTipo = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dgvMostrar)).BeginInit();
             this.gbInsertar.SuspendLayout();
             this.menuStrip1.SuspendLayout();
@@ -54,7 +60,7 @@ namespace Diccionario_Personal
             // dgvMostrar
             // 
             this.dgvMostrar.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvMostrar.Location = new System.Drawing.Point(192, 27);
+            this.dgvMostrar.Location = new System.Drawing.Point(190, 90);
             this.dgvMostrar.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.dgvMostrar.Name = "dgvMostrar";
             this.dgvMostrar.Size = new System.Drawing.Size(548, 285);
@@ -62,17 +68,18 @@ namespace Diccionario_Personal
             // 
             // bttnGuardar
             // 
-            this.bttnGuardar.Location = new System.Drawing.Point(27, 242);
+            this.bttnGuardar.Location = new System.Drawing.Point(27, 323);
             this.bttnGuardar.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.bttnGuardar.Name = "bttnGuardar";
             this.bttnGuardar.Size = new System.Drawing.Size(100, 25);
             this.bttnGuardar.TabIndex = 1;
             this.bttnGuardar.Text = "Guardar";
             this.bttnGuardar.UseVisualStyleBackColor = true;
+            this.bttnGuardar.Click += new System.EventHandler(this.bttnGuardar_Click);
             // 
             // tbxPalabra
             // 
-            this.tbxPalabra.Location = new System.Drawing.Point(10, 38);
+            this.tbxPalabra.Location = new System.Drawing.Point(13, 124);
             this.tbxPalabra.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.tbxPalabra.Name = "tbxPalabra";
             this.tbxPalabra.Size = new System.Drawing.Size(132, 22);
@@ -80,7 +87,7 @@ namespace Diccionario_Personal
             // 
             // tbxTraduccion
             // 
-            this.tbxTraduccion.Location = new System.Drawing.Point(10, 84);
+            this.tbxTraduccion.Location = new System.Drawing.Point(13, 170);
             this.tbxTraduccion.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.tbxTraduccion.Name = "tbxTraduccion";
             this.tbxTraduccion.Size = new System.Drawing.Size(132, 22);
@@ -88,6 +95,10 @@ namespace Diccionario_Personal
             // 
             // gbInsertar
             // 
+            this.gbInsertar.Controls.Add(this.lbl_Idioma);
+            this.gbInsertar.Controls.Add(this.cbxTipo);
+            this.gbInsertar.Controls.Add(this.cbxIdioma);
+            this.gbInsertar.Controls.Add(this.lblTipo);
             this.gbInsertar.Controls.Add(this.rtbIdea);
             this.gbInsertar.Controls.Add(this.lbl_Idea);
             this.gbInsertar.Controls.Add(this.bttnGuardar);
@@ -97,36 +108,44 @@ namespace Diccionario_Personal
             this.gbInsertar.Controls.Add(this.tbxTraduccion);
             this.gbInsertar.Location = new System.Drawing.Point(12, 27);
             this.gbInsertar.Name = "gbInsertar";
-            this.gbInsertar.Size = new System.Drawing.Size(173, 285);
+            this.gbInsertar.Size = new System.Drawing.Size(173, 355);
             this.gbInsertar.TabIndex = 7;
             this.gbInsertar.TabStop = false;
             // 
-            // lblPalabra
+            // rtbIdea
             // 
-            this.lblPalabra.AutoSize = true;
-            this.lblPalabra.Location = new System.Drawing.Point(10, 19);
-            this.lblPalabra.Name = "lblPalabra";
-            this.lblPalabra.Size = new System.Drawing.Size(55, 14);
-            this.lblPalabra.TabIndex = 5;
-            this.lblPalabra.Text = "Palabra";
+            this.rtbIdea.Location = new System.Drawing.Point(13, 221);
+            this.rtbIdea.Name = "rtbIdea";
+            this.rtbIdea.Size = new System.Drawing.Size(129, 96);
+            this.rtbIdea.TabIndex = 8;
+            this.rtbIdea.Text = "";
+            // 
+            // lbl_Idea
+            // 
+            this.lbl_Idea.AutoSize = true;
+            this.lbl_Idea.Location = new System.Drawing.Point(10, 200);
+            this.lbl_Idea.Name = "lbl_Idea";
+            this.lbl_Idea.Size = new System.Drawing.Size(106, 14);
+            this.lbl_Idea.TabIndex = 7;
+            this.lbl_Idea.Text = "Idea inverosímil";
             // 
             // lblTraduccion
             // 
             this.lblTraduccion.AutoSize = true;
-            this.lblTraduccion.Location = new System.Drawing.Point(10, 67);
+            this.lblTraduccion.Location = new System.Drawing.Point(13, 153);
             this.lblTraduccion.Name = "lblTraduccion";
             this.lblTraduccion.Size = new System.Drawing.Size(73, 14);
             this.lblTraduccion.TabIndex = 6;
             this.lblTraduccion.Text = "Traducción";
             // 
-            // lbl_Idea
+            // lblPalabra
             // 
-            this.lbl_Idea.AutoSize = true;
-            this.lbl_Idea.Location = new System.Drawing.Point(10, 119);
-            this.lbl_Idea.Name = "lbl_Idea";
-            this.lbl_Idea.Size = new System.Drawing.Size(106, 14);
-            this.lbl_Idea.TabIndex = 7;
-            this.lbl_Idea.Text = "Idea inverosímil";
+            this.lblPalabra.AutoSize = true;
+            this.lblPalabra.Location = new System.Drawing.Point(13, 105);
+            this.lblPalabra.Name = "lblPalabra";
+            this.lblPalabra.Size = new System.Drawing.Size(55, 14);
+            this.lblPalabra.TabIndex = 5;
+            this.lblPalabra.Text = "Palabra";
             // 
             // menuStrip1
             // 
@@ -149,6 +168,18 @@ namespace Diccionario_Personal
             this.archivoToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
             this.archivoToolStripMenuItem.Text = "Archivo";
             // 
+            // guardarToolStripMenuItem
+            // 
+            this.guardarToolStripMenuItem.Name = "guardarToolStripMenuItem";
+            this.guardarToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.guardarToolStripMenuItem.Text = "Guardar";
+            // 
+            // salirToolStripMenuItem
+            // 
+            this.salirToolStripMenuItem.Name = "salirToolStripMenuItem";
+            this.salirToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.salirToolStripMenuItem.Text = "Salir";
+            // 
             // verToolStripMenuItem
             // 
             this.verToolStripMenuItem.Name = "verToolStripMenuItem";
@@ -164,43 +195,83 @@ namespace Diccionario_Personal
             this.preferenciasToolStripMenuItem.Size = new System.Drawing.Size(83, 20);
             this.preferenciasToolStripMenuItem.Text = "Preferencias";
             // 
-            // guardarToolStripMenuItem
-            // 
-            this.guardarToolStripMenuItem.Name = "guardarToolStripMenuItem";
-            this.guardarToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.guardarToolStripMenuItem.Text = "Guardar";
-            // 
-            // salirToolStripMenuItem
-            // 
-            this.salirToolStripMenuItem.Name = "salirToolStripMenuItem";
-            this.salirToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.salirToolStripMenuItem.Text = "Salir";
-            // 
             // configuracionesToolStripMenuItem
             // 
             this.configuracionesToolStripMenuItem.Name = "configuracionesToolStripMenuItem";
-            this.configuracionesToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.configuracionesToolStripMenuItem.Size = new System.Drawing.Size(161, 22);
             this.configuracionesToolStripMenuItem.Text = "Configuraciones";
             // 
             // ayudaToolStripMenuItem
             // 
             this.ayudaToolStripMenuItem.Name = "ayudaToolStripMenuItem";
-            this.ayudaToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.ayudaToolStripMenuItem.Size = new System.Drawing.Size(161, 22);
             this.ayudaToolStripMenuItem.Text = "Ayuda";
             // 
-            // rtbIdea
+            // cbxIdioma
             // 
-            this.rtbIdea.Location = new System.Drawing.Point(13, 140);
-            this.rtbIdea.Name = "rtbIdea";
-            this.rtbIdea.Size = new System.Drawing.Size(129, 96);
-            this.rtbIdea.TabIndex = 8;
-            this.rtbIdea.Text = "";
+            this.cbxIdioma.FormattingEnabled = true;
+            this.cbxIdioma.Items.AddRange(new object[] {
+            "Ingles - Español"});
+            this.cbxIdioma.Location = new System.Drawing.Point(13, 38);
+            this.cbxIdioma.Name = "cbxIdioma";
+            this.cbxIdioma.Size = new System.Drawing.Size(132, 22);
+            this.cbxIdioma.TabIndex = 9;
+            // 
+            // lbl_Idioma
+            // 
+            this.lbl_Idioma.AutoSize = true;
+            this.lbl_Idioma.Location = new System.Drawing.Point(10, 18);
+            this.lbl_Idioma.Name = "lbl_Idioma";
+            this.lbl_Idioma.Size = new System.Drawing.Size(50, 14);
+            this.lbl_Idioma.TabIndex = 9;
+            this.lbl_Idioma.Text = "Idioma";
+            // 
+            // lblTipo
+            // 
+            this.lblTipo.AutoSize = true;
+            this.lblTipo.Location = new System.Drawing.Point(13, 63);
+            this.lblTipo.Name = "lblTipo";
+            this.lblTipo.Size = new System.Drawing.Size(33, 14);
+            this.lblTipo.TabIndex = 10;
+            this.lblTipo.Text = "Tipo";
+            // 
+            // cbxTipo
+            // 
+            this.cbxTipo.FormattingEnabled = true;
+            this.cbxTipo.Items.AddRange(new object[] {
+            "Sustantivo",
+            "Verbo",
+            "Pronombre"});
+            this.cbxTipo.Location = new System.Drawing.Point(13, 80);
+            this.cbxTipo.Name = "cbxTipo";
+            this.cbxTipo.Size = new System.Drawing.Size(132, 22);
+            this.cbxTipo.TabIndex = 10;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(576, 34);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(50, 50);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Del";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(520, 34);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(50, 50);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Edit";
+            this.button2.UseVisualStyleBackColor = true;
             // 
             // FormMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 14F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(751, 331);
+            this.ClientSize = new System.Drawing.Size(751, 392);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.gbInsertar);
             this.Controls.Add(this.dgvMostrar);
             this.Controls.Add(this.menuStrip1);
@@ -239,6 +310,12 @@ namespace Diccionario_Personal
         private System.Windows.Forms.ToolStripMenuItem preferenciasToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem configuracionesToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem ayudaToolStripMenuItem;
+        private System.Windows.Forms.Label lbl_Idioma;
+        private System.Windows.Forms.ComboBox cbxTipo;
+        private System.Windows.Forms.ComboBox cbxIdioma;
+        private System.Windows.Forms.Label lblTipo;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
---
+++ archivoXML.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Diccionario_Personal
+{
+    class archivoXML
+    {
+        public void creatingXML(XmlDocument doc)
+        {
+
+            XmlDeclaration init = doc.CreateXmlDeclaration("1.0", "UTF-8", null);
+            XmlElement root = doc.DocumentElement;
+            doc.InsertBefore(init, root);
+
+            XmlElement element1 = doc.CreateElement(string.Empty, "cuerpo", string.Empty);
+            doc.AppendChild(element1);
+        }
+    }
+}
